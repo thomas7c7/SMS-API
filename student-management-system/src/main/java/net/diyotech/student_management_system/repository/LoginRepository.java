44ff111847e4bbdc9ab1f77fa69d46b0f1407f52
Tilak Basnet <admin@macbook-pro.mynetworksettings.com>
2025-02-27 20:04:18 -0500

    TICKET123 Implemented login functionality

---
+++ student-management-system/src/main/java/net/diyotech/student_management_system/repository/LoginRepository.java
@@ -0,0 +1,9 @@
+package net.diyotech.student_management_system.repository;
+
+import net.diyotech.student_management_system.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoginRepository extends JpaRepository<User, Long> {
+}
--- student-management-system/src/main/java/net/diyotech/student_management_system/service/LoginService.java
+++ student-management-system/src/main/java/net/diyotech/student_management_system/service/LoginService.java
@@ -1,12 +1,18 @@
 package net.diyotech.student_management_system.service;
 
+import lombok.RequiredArgsConstructor;
 import net.diyotech.student_management_system.model.User;
+import net.diyotech.student_management_system.repository.LoginRepository;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class LoginService {
 
+    private final LoginRepository loginRepository;
+
     public String login(User user) {
+      loginRepository.save(user);
       return "login successful";
     }
 }
